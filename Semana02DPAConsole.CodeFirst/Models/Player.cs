e943db0b2007e1d70c9845f56cce0e246c7c2ede
LENOVO <LENOVO@LENOVO>
2022-09-11 12:12:21 -0500

    Agregar archivos de proyecto.

---
+++ Semana02DPAConsole.CodeFirst/Models/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Semana02DPAConsole.CodeFirst.Models
+{
+    [Table ("Player")]
+    public class Player //clase con dos atributos
+    {
+        [Key]
+        //prop tab tab
+        public int Id { get; set; }
+        [Column (TypeName = "nvarchar(100)")]
+        public string FullName { get; set; }
+
+
+
+    }
+}
---
+++ Semana02DPAConsole.CodeFirst/Models/Qatar2022DbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana02DPAConsole.CodeFirst.Models
+{
+    public class Qatar2022DbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            //Windows authentications
+            var myCnxString = "Server=LENOVO\\SQLEXPRESS;Database=Qatar2022Db;Integrated Security = true"; //ira en un json luego
+            // SQL auth
+            //var myCnxString = "Server=MYSERVER;Database=MYDATABASE;User=MYUSER;Password=MYPASSWORD"; 
+
+
+            options
+                .UseSqlServer(myCnxString)
+                .LogTo(Console.WriteLine, LogLevel.Information);
+
+
+        }
+
+        public virtual DbSet<Player> Player { get; set; } //como voy a llamar a la clase Player con sus metodos get and set
+
+
+
+
+    }
+}
